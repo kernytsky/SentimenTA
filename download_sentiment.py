b5407df53aca451877343fd26f82c5333242a03a
Andrew Kernytsky <kernytsky@gmail.com>
2025-03-24 17:53:12 -0400

    Make download path relative

--- download_sentiment.py
+++ download_sentiment.py
@@ -4,6 +4,7 @@ import pandas as pd
 from wsb_scanner import WSBScanner
 #wsb_scanner import get_swaggy_stocks, get_ape_wisdom, get_trending_stocks
 from datetime import datetime
+from pathlib import Path
 
 # Call the functions and get the data frames
 scanner = WSBScanner()
@@ -16,5 +17,7 @@ st = scanner.get_trending_stocks(limit)
 timestamp = datetime.now().strftime('%y%m%d_%H%M%S')
 
 # Save the data frames to CSV files with timestamp
-aw.to_csv(f'/Users/andrew/projects/trade/SentimenTA/data/{timestamp}_apewisdom.csv', index=False)
-st.to_csv(f'/Users/andrew/projects/trade/SentimenTA/data/{timestamp}_stocktwits.csv', index=False)
+directory_path = Path("data")
+directory_path.mkdir(parents=True, exist_ok=True)
+aw.to_csv(f'data/{timestamp}_apewisdom.csv', index=False)
+st.to_csv(f'data/{timestamp}_stocktwits.csv', index=False)
